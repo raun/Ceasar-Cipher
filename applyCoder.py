b440850dd96302a7ce2d752008c7c71985f2f00a
Raun <raun@C700.(none)>
2012-11-03 16:31:13 +0530

    Edited file

--- applyCoder.py
+++ applyCoder.py
@@ -1,17 +1,4 @@
 import string
-def buildCoder(shift):
-    mapper={}
-    for ch in string.ascii_lowercase:
-        if (ord(ch)+shift)>ord('z'):
-            mapper[ch]=chr(ord(ch)+shift-ord('z')+ord('a')-1)
-        else:
-            mapper[ch]=chr(ord(ch)+shift)
-    for ch in string.ascii_uppercase:
-        if (ord(ch)+shift)>ord('Z'):
-            mapper[ch]=chr(ord(ch)+shift-ord('Z')+ord('A')-1)
-        else:
-            mapper[ch]=chr(ord(ch)+shift)
-    return mapper
 def applyCoder(text, coder):
     """
     Applies the coder to the text. Returns the encoded text.
